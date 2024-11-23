e7a89e2f1d141467b4ab7e5e47c96e82e994a1be
Rafael Rodrigues <155591971+rafaelcodesrr@users.noreply.github.com>
2024-11-22 22:33:13 -0300

    Update index.js
    corrigindo O operador lógico &&

--- src/index.js
+++ src/index.js
@@ -11,9 +11,9 @@ if (xp < 1000 ){
     nivel = "Prata"
 }else if (xp >= 5001 && xp <=7000){
     nivel = "Ouro"
-}else if (xp >= 7001 & xp <=8000){
+}else if (xp >= 7001 && xp <=8000){
     nivel = "Platina"
-}else if(xp >= 8001 & xp<= 9000){
+}else if(xp >= 8001 && xp<= 9000){
     nivel = "Ascendente"
 }else if (xp >= 9001 && xp<= 10000){
     nivel = "Imortal"
